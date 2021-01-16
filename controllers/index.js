29913c351ca0d95b7e0a679cb11baa0c7e7937a6
hadi-mirza <hadimirza1@gmail.com>
2021-01-15 23:27:57 -0500

    req.ip test

--- controllers/index.js
+++ controllers/index.js
@@ -1,6 +1,6 @@
 function index(req, res, next) {
   var ip = req.headers['x-forwarded-for'] || req.connection.remoteAddress;
-  res.render("index", {userIp, ip});
+  res.render("index", {ip});
 }
 
 module.exports = {
